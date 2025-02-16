c274aecbbc5dce884bc49fb63a00054cfacb0594
Ken'ichi Fukamachi <fukachan@fml.org>
2025-02-16 14:09:51 +0900

    - fixed to fit v2.0.0-alpha.2 repository structure (contentx/${cmd}.re)

--- utils/v2-aggregate-files.sh
+++ utils/v2-aggregate-files.sh
@@ -6,10 +6,13 @@ cd $(dirname $0)/.. || exit 1
 
 pwd
 
-list=$(awk '{print "contents/" $2}' etc/list.v2)
+# obsolete (< v2.0.0-alpha.1)
+# list=$(awk '{print "contents/" $2}' etc/list.v2)
+# for f in $list; do .... ; done
 
+# >= v2.0.0-alpha.2 (after $cmd.re relocation)
 cat contents/v1/10-selected.re > $dst
-for f in $list
+for f in contents/[a-z]*.re
 do
   (
     perl -nple 'if (/^=/) { $count = 2 }; print "\n\n" if $count-- > 0' $f
